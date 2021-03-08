305ab289154da67822d2a5b718b7b92d21a64f29
Minh Ngo <ngo.minh0224@gmail.com>
2021-03-08 11:09:14 -0800

    homepage

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-export default class home extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+export default class HomePage extends Component {
+  render() {
+    return (
+      <div className="credentials">
+        <input className="input">Username:</input>
+        <input className="input">Password:</input>
+        <button type="submit">Log In</button>
+        <button type="submit">Create New Account</button>
+      </div>
+    );
+  }
 }
---
+++ src/SearchPage/Search_Page.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class Search_Page extends Component {
+  render() {
+    return <div></div>;
+  }
+}
