573761f1e4e7022c9d2778064b5781fb828ffd4d
Tyler Farris <Tylerpfarris@gmail.com>
2021-03-08 11:51:34 -0800

    Basic Header and Nav bar made- logic to hide favorites page and signout button in there too

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom'
+import style from './Header.module.css'
 
 export default class Header extends Component {
     render() {
         return (
-            <div>
+            <div className={style.headerContainerDiv}>
+                <div className={style.linkContainerDiv}>
+                <NavLink to="/"><p>Home</p></NavLink> 
+                <NavLink to="/search"><p>Search</p></NavLink> 
+                <NavLink to="/favorites"><p>favorites</p></NavLink> 
+                        <button onClick={this.props.handleLogOut}>Sign Out</button>
+                {/* {
+                    this.props.user && this.props.user.token &&
+                    <>
+                        
+                    </>
+                } */}
                 
+             
+                
+            </div>
             </div>
         )
     }
