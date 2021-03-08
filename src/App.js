dae9c9181c1163de194d8692cbeba7281e74a16e
zhaned <zhanedmond@gmail.com>
2021-03-08 13:31:54 -0800

    added search page results and search functionality

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
-import React, { Component } from 'react'
-import {
-    BrowserRouter as Router, 
-    Route, 
-    Switch,
-} from 'react-router-dom';
-import PrivateRoute from './Components/PrivateRoute';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import PrivateRoute from './Components/PrivateRoute';
 
 import './App.css';
 
-import { getUserFromLocalStorage, putUserInLocalStorage } from './local-storage-utils.js';
+// import { getUserFromLocalStorage, putUserInLocalStorage } from './local-storage-utils.js';
 
 import Header from './Components/Header';
 import Home from './Home/Home';
 import Favorites from './Favorites/Favorites';
-import Search_Page from './SearchPage/Search_Page'
-
-
-
+import SearchPage from './SearchPage/SearchPage';
 
 export default class App extends Component {
   state = {
     user: [],
-      // getUserFromLocalStorage()
-  }
+    // getUserFromLocalStorage()
+  };
 
   // handleUserChange = (user) => {
   //   this.setState({ user });
@@ -32,44 +25,48 @@ export default class App extends Component {
 
   // handleLogOut = () => {
   //   this.handleUserChange();
-    
+
   // }
 
   render() {
     const { user } = this.state;
-        return (
-            <div>
-                <Router>
-                  <Header
-                    // user={user}
-                    // handleLogOut={this.handleLogOut}
-                    />
-                    <Switch>
-                        <Route 
-                            path="/" 
-                            exact
-                            render={(routerProps) => <Home {...routerProps} />} 
-                        />
-                        <PrivateRoute 
-                            path="/favorites" 
-                            exact
-                            // token={user && user.token}
-                            render={(routerProps) =>
-                            <Favorites
-                                // user={user}
-                                {...routerProps} />} 
-                        />
-                        <Route 
-                            path="/search" 
-                            exact
-                            render={(routerProps) =>
-                            <Search_Page
-                                // user={user}
-                                {...routerProps} />} 
-                        />
-                    </Switch>
-                </Router>
-            </div>
-        )
-    }
+    return (
+      <div>
+        <Router>
+          <Header
+          // user={user}
+          // handleLogOut={this.handleLogOut}
+          />
+          <Switch>
+            <Route
+              path="/"
+              exact
+              render={(routerProps) => <Home {...routerProps} />}
+            />
+            <Route
+              path="/favorites"
+              exact
+              // token={user && user.token}
+              render={(routerProps) => (
+                <Favorites
+                  // user={user}
+                  {...routerProps}
+                />
+              )}
+            />
+            <Route
+              path="/search"
+              exact
+              render={(routerProps) => (
+                <SearchPage
+                  // user={user}
+                  {...routerProps}
+                />
+              )}
+            />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
