d21c4ddbea5e8ac964b157fb01b55e769cf338e6
Eion Nelson <ecnelson1@live.com>
2021-03-08 09:32:39 -0800

    initial file tree

---
+++ src/SearchPage/Search-Page.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Search-Page extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
